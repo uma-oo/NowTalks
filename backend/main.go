17d2f28227ceb9ecad010c011bc76a19498085a8
uma-oo <oumayma.elfahsi99@gmail.com>
2025-05-04 16:29:02 +0100

    add some code

--- backend/main.go
+++ backend/main.go
@@ -1,4 +1,24 @@
 package main
 
-func main(){
+import (
+	"fmt"
+
+	database "real-time-forum/backend/database"
+)
+
+func main() {
+	db, err := database.InitDB("./database/forum.db")
+	if err != nil {
+		fmt.Println("err", err)
+	}
+    err = db.ReadSQL("./database/db.sql")
+	if err != nil {
+		fmt.Println("err", err)
+	}
+	defer db.Database.Close()
+	_ , err = db.Database.Exec(`INSERT INTO categories (category) values ("IT");`)
+	fmt.Println("res" ,err)
+
+
+
 }
