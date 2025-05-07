2693116eb5cb7a59675c6a61061703b0470acf30
DarkMethoss <ayoubstudies01@gmail.com>
2025-05-07 17:53:18 +0100

    .

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,1 +1,13 @@
-export function Header
+export function createHeader(userName) {
+    let header = document.createElement('header')
+    let logo = document.createElement('a')
+    logo.href = '/'
+    logo.textContent = 'Talkaa'
+    logo.className = 'logo'
+
+    
+
+
+    
+    
+}
