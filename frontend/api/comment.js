e79e9a6f9ffb4ae965fb10fae05d8c8ed532753e
uma-oo <oumayma.elfahsi99@gmail.com>
2025-05-09 16:32:50 +0100

    .

--- frontend/api/comment.js
+++ frontend/api/comment.js
@@ -1,11 +1,17 @@
 
-export async function getComments() {
-    const response = await fetch('/api/comments');
+
+
+// You have to specify the id assi ayoub 
+export async function getComments(id) {
+    const response = await fetch(`/api/comment?postId=${id}`);
     return response.json();
 }
 
+
+
+// the same applies for the creation !!! khass id f l api 
 export async function createComment(commentData) {
-    const response = await fetch('/api/comments', {
+    const response = await fetch('/api/comment', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(commentData),
