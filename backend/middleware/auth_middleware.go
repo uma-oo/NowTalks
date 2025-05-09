29031f8517f4402db6fd0ce4c44cb2a127dae44e
uma-oo <oumayma.elfahsi99@gmail.com>
2025-05-09 17:13:18 +0100

    .

--- backend/middleware/auth_middleware.go
+++ backend/middleware/auth_middleware.go
@@ -2,10 +2,12 @@ package middleware
 
 import (
 	"net/http"
-
 	"real-time-forum/backend/models"
+	handler "real-time-forum/backend/handler"
+
 )
 
+// could be returning a boolean but to see again
 func (m *Middleware) GetAuthUser(r *http.Request) (*models.Session, *models.ErrorJson) {
 	cookie, err := r.Cookie("session")
 	if err != nil {
@@ -17,4 +19,13 @@ func (m *Middleware) GetAuthUser(r *http.Request) (*models.Session, *models.Erro
 		return nil, errJson
 	}
 	return session, nil
+}
+
+func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	_, err := m.GetAuthUser(r)
+	if err != nil {
+		handler.WriteJsonErrors(w, *err)
+		return
+	}
+	m.MiddlewareHanlder.ServeHTTP(w, r)
 }
--- backend/middleware/middleware.go
+++ backend/middleware/middleware.go
@@ -2,8 +2,6 @@ package middleware
 
 import (
 	"net/http"
-
-	handler "real-time-forum/backend/handler"
 	"real-time-forum/backend/service"
 )
 
@@ -13,15 +11,6 @@ import (
 type Middleware struct {
 	MiddlewareHanlder http.Handler
 	service *service.AppService
-}
-
-func (m *Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_ , err := m.GetAuthUser(r)
-	if err != nil {
-		handler.WriteJsonErrors(w, *err)
-		return
-	}
-	m.MiddlewareHanlder.ServeHTTP(w,r)
 }
 
 func NewMiddleWare(handler http.Handler, service *service.AppService) *Middleware {
