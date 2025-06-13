645d2bc14553d6cdaaa5b394ede81fe797690a39
DarkMethoss <ayoubstudies01@gmail.com>
2025-06-13 12:44:05 +0100

    fix comment like reaction

--- frontend/components/comment.js
+++ frontend/components/comment.js
@@ -16,11 +16,14 @@ export function createComment({
     let commentWriter = createElement('span', null, user_name)
     let commentCreatedAt = createElement('span', null, timeAgo(created_at))
     let commentText = createElement('p', null, content)
+
+    let reaction = createElement('div', 'reaction-container')
     let heartIcon = createIcon("heart", "like")
+    let count = createElement('span', null, total_likes ? total_likes : '0')
+    
     if (liked != 0) {
         heartIcon.style.fill = "red"
     }
-    let count = createElement('span', null, total_likes ? total_likes : '0')
     heartIcon.addEventListener("click", () => {
         ToggleLike(
             {
@@ -32,7 +35,8 @@ export function createComment({
     })
     commentWriter.prepend(createIcon("user"))
     commentCreatedAt.prepend(createIcon("calendar"))
+    reaction.append(heartIcon, count)
     commentContent.append(commentWriter, commentCreatedAt, commentText)
-    commentContainer.append(commentContent, heartIcon, count)
+    commentContainer.append(commentContent, reaction)
     return commentContainer
 }
